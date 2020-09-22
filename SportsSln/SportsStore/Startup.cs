52ebd1bbb6adb9c4f2416b85b492956d35432116
student <student@DESKTOP-L3ET5DL>
2020-09-22 20:50:01 +0300

    identity step 1

--- SportsSln/SportsStore/Startup.cs
+++ SportsSln/SportsStore/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,14 +32,22 @@ namespace SportsStore {
             // обеспечение внедрения зависимостей через конструктор всем контроллерам
             // и службам, у которых в конструкторах есть соответвующие аргументы
             services.AddScoped<IStoreRepository, EFStoreRepository> ();
-            services.AddScoped<IOrderRepository, EFOrderRepository>();
+            services.AddScoped<IOrderRepository, EFOrderRepository> ();
             // активация дополнительного мини-каркаса работы с серверными страницами
-            services.AddRazorPages();
+            services.AddRazorPages ();
             // активация работы с http-сеансами
-            services.AddDistributedMemoryCache();
-            services.AddSession();
-            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddDistributedMemoryCache ();
+            services.AddSession ();
+            services.AddScoped<Cart> (sp => SessionCart.GetCart (sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor> ();
+            services.AddServerSideBlazor ();
+
+            // Identity
+            services.AddDbContext<AppIdentityDbContext> (options =>
+                options.UseSqlServer (
+                    Configuration["ConnectionStrings:IdentityConnection"]));
+            services.AddIdentity<IdentityUser, IdentityRole> ()
+                .AddEntityFrameworkStores<AppIdentityDbContext> ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,8 +59,14 @@ namespace SportsStore {
             в PipeLine приложения */
             app.UseStatusCodePages ();
             app.UseStaticFiles ();
-            app.UseSession();
+            app.UseSession ();
             app.UseRouting ();
+
+            // Identity
+            // Вход / Выход
+            app.UseAuthentication();
+            // Допуск / Недопуск к ресурсам
+            app.UseAuthorization();
 
             app.UseEndpoints (endpoints => {
                 /* endpoints.MapControllerRoute("pagination",
@@ -78,7 +93,9 @@ namespace SportsStore {
                 // добавление роута по умолчанию
                 endpoints.MapDefaultControllerRoute ();
                 // добавление роута серверных страниц
-                endpoints.MapRazorPages();
+                endpoints.MapRazorPages ();
+                endpoints.MapBlazorHub ();
+                endpoints.MapFallbackToPage ("/admin/{*catchall}", "/Admin/Index");
                 // The same:
                 /* endpoints.MapControllerRoute("pagination",
                     "Products/{Page?}/{productPage:regex(^[1-9]\\d*$)}",
@@ -87,8 +104,10 @@ namespace SportsStore {
                 }); */
                 // endpoints.MapDefaultControllerRoute ();
             });
-
+            // Заполнение БД демонстрационными данными
             SeedData.EnsurePopulated (app);
+            // Заполнение БД демонстрационными данными системы безопасности Identity
+            IdentitySeedData.EnsurePopulated(app);
         }
     }
 }
